e430fb8cf453384c7d32a0a2be4645fe174378c6
KheireddineDerbali <kheireddinederbali@gmail.com>
2026-02-11 10:09:08 +0100

    simple dynamic list display

--- demo/views.py
+++ demo/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    context = {
+        'title': 'Home Page', 
+        'message': 'Welcome to the Home Page!',
+        'fruits': ['Apple', 'Banana', 'Cherry', 'Date', 'Elderberry']
+    }
+    return render(request, 'home.html', context)
+
+def fruits(request):
+    fruits_list = ['Apple', 'Banana', 'Cherry', 'Date', 'Elderberry']
+
+    context = {
+        'title': 'Fruits Page',
+        'message': 'Here is a list of fruits.',
+        'fruits': fruits_list
+    }
+
+    return render(request, 'fruits.html', context)
