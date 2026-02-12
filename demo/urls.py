37acc561329b3753f6b2e547b2f5ecb21794d9bb
KheireddineDerbali <kheireddinederbali@gmail.com>
2026-02-12 09:11:24 +0100

    heritage concept

--- demo/urls.py
+++ demo/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('fruits/', views.fruits, name='fruits'),
+    path('filtres/', views.filtres, name='filtres'),
+    
+    # héritage (nouvelle partie)
+    path('heritage/page1/', views.heritage_page1, name='heritage_page1'),
+    path('heritage/page2/', views.heritage_page2, name='heritage_page2'),
 ]
--- demo/views.py
+++ demo/views.py
@@ -1,4 +1,6 @@
+from urllib import request
 from django.shortcuts import render
+from datetime import date
 
 def home(request):
     context = {
@@ -18,3 +20,26 @@ def fruits(request):
     }
 
     return render(request, 'fruits.html', context)
+def filtres(request):
+    contexte = {
+        "nom": "django framework",
+        "phrase": "bonjour tout le monde",
+        "texte": "Django est un framework web puissant et sécurisé",
+        "fruits": ["Pomme", "Banane", "Orange"],
+        "prix": 100,
+        "note": 14.5678,
+        "date": date.today(),
+        "actif": True,
+        "contenu_html": "<strong>Texte HTML autorisé</strong>",
+        "vide": None,
+    }
+    return render(request, "filtres.html", contexte)
+
+def heritage_page1(request):
+    return render(request, 'heritage/page1.html')
+
+def heritage_page2(request):
+    contexte = {
+        "items": ["Django", "Templates", "Héritage"]
+    }
+    return render(request, 'heritage/page2.html', contexte)
